73e0e2b9fc08618997795ef840d5e9723c099f58
Acosticahub <acostabuitrago0123@gmail.com>
2021-10-29 14:07:58 -0500

    Front-Perfil

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -13,35 +13,52 @@
 <body>
     <div class="container contenedor-perfil rounded bg-white mt-5 mb-5">
         <div class="row">
-            <div class="col-md-3 border-right">
+            <div class="col-md-3 col-sm-6 border-right">
                 <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="imagen-perfil mt-3" src="https://cdn-icons-png.flaticon.com/512/2416/2416668.png"><br><span class="font-weight-bold">{{Auth::user()->rol->nomrol}}</span><span class="text-black-50">{{Auth::user()->email}}</span><span> </span></div>
             </div>
-            <div class="col-md-7">
+            <div class="col-md-9 col-sm-6">
                 <div class="p-3 py-5">
                     <div class="d-flex justify-content-between align-items-center mb-3">
                         <h3 class="text-right">Editar Perfil</h3>
                     </div>
                     <div class="row mt-2">
                         <div class="col-md-6">
-                        <select name="typeid_id" class="form-control" required>
-                            <option>Seleccione Tipo de Identificacion*</option>
-                            <option value="{{Auth::user()->typeid_id}}" selected>{{Auth::user()->typeid->nomtypeid}}</option>
-                            @foreach ($identificaciones as $identidicacion)
-                            <option value="{{$identidicacion->idtypeid}}">{{ $identidicacion->nomtypeid }}</option>
-                            @endforeach
-                        </select>
+                            <label class="labels">Tipo de Identificación</label>
+                            <select name="typeid_id" class="form-control form-control-user" id="campo" required>
+                                <option disabled>Seleccione Tipo de Identificación *</option>
+                                <option value="{{Auth::user()->typeid_id}}" selected>{{Auth::user()->typeid->nomtypeid}}</option>
+                                @foreach ($identificaciones as $identidicacion)
+                                <option value="{{$identidicacion->idtypeid}}">{{ $identidicacion->nomtypeid }}</option>
+                                @endforeach
+                            </select>
                         </div>
-                        <div class="col-md-6"><label class="labels">Número de Identificación</label><input type="text" class="form-control" value="{{Auth::user()->numid}}" placeholder="Número de Identificación"></div>
+                        <div class="col-md-6"><label class="labels">Número de Identificación</label>
+                            <input type="text" class="form-control form-control-user" value="{{Auth::user()->numid}}" placeholder="Número de Identificación">
+                        </div>
                     </div>
                     <div class="row mt-3">
-                        <div class="col-md-12"><label class="labels">Nombre Completo</label><input type="text" class="form-control" placeholder="Nombre Completo" value="{{Auth::user()->nomuser}}"></div>
-                        <div class="col-md-12"><label class="labels">Apellidos</label><input type="text" class="form-control" placeholder="Apellidos" value="{{Auth::user()->apeuser}}"></div>
-                        <div class="col-md-12"><label class="labels">Correo Electrónico</label><input type="text" class="form-control" placeholder="Correo Electrónico" value="{{Auth::user()->email}}"></div>
-                        <div class="col-md-12"><label class="labels">NickName</label><input type="text" class="form-control" placeholder="NickName" value="{{Auth::user()->nick}}"></div>
+                        <div class="col-md-6"><label class="labels">Nombre Completo</label>
+                            <input type="text" class="form-control form-control-user" placeholder="Nombre Completo" value="{{Auth::user()->nomuser}}">
+                        </div>
+                        <div class="col-md-6"><label class="labels">Apellidos</label>
+                            <input type="text" class="form-control form-control-user" placeholder="Apellidos" value="{{Auth::user()->apeuser}}">
+                        </div>
                     </div>
                     <div class="row mt-3">
-                        <div class="col-md-6"><label class="labels">Contraseña</label><input type="text" class="form-control" placeholder="Nueva contraseña" value=""></div>
-                        <div class="col-md-6"><label class="labels">Confirmar Contraseña</label><input type="text" class="form-control" value="" placeholder="Confirmar contraseña"></div>
+                        <div class="col-md-6"><label class="labels">Correo Electrónico</label>
+                            <input type="text" class="form-control form-control-user" placeholder="Correo Electrónico" value="{{Auth::user()->email}}">
+                        </div>
+                        <div class="col-md-6"><label class="labels">NickName</label>
+                            <input type="text" class="form-control form-control-user" placeholder="NickName" value="{{Auth::user()->nick}}">
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-md-6"><label class="labels">Contraseña</label>
+                            <input type="text" class="form-control form-control-user" placeholder="Nueva contraseña" value="">
+                        </div>
+                        <div class="col-md-6"><label class="labels">Confirmar Contraseña</label>
+                            <input type="text" class="form-control form-control-user" value="" placeholder="Confirmar contraseña">
+                        </div>
                     </div>
                     <div class="mt-5 text-center"><button class="btn btn-primary btn-user btn-block" type="button">Guardar Perfil</button></div>
                 </div>
