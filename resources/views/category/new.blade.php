f942600aa132cfcac4ee60bc0b30d64f578ffd29
Acosticahub <acostabuitrago0123@gmail.com>
2021-10-30 10:40:32 -0500

    Responsive Category

--- resources/views/category/new.blade.php
+++ resources/views/category/new.blade.php
@@ -1,3 +1,4 @@
+@extends('layaut')
 <!DOCTYPE html>
 <html>
 
@@ -11,14 +12,40 @@
 
 <body>
     <div class="container">
-        <a href="../category" class="btn btn-secondary float-end mt-2"> Volver </a>
-        <h1>Agregar Categoría</h1>
-        <hr>
-        <form action="{{url('category')}}" method="POST">
+        <a href="{{ url('category') }}" class="btn btn-danger float-right"><i class="fas fa-window-close"></i> Cancelar</a>
+        <!-- Encabezado de página -->
+        <h1 class="h3 mb-4 text-gray-800">Agregar una Categoría</h1>
+
+        <form action="{{url('category')}}" method="POST" class="user">
             @csrf
-            <input name="nomcategory" type="text" class="form-control form-control-user" placeholder="Nombre De La Categoría*" autofocus>
-            <br>
-            <input type="submit" class="btn btn-success">
+            <!-- Fila -->
+            <div class="row">
+                <div class="col-md-1 col-sm-1"></div>
+                <!-- Columna -->
+                <div class="col-md-10 col-sm-10">
+                    <div class="p-3 py-5">
+
+                        <!-- Formulario -->
+                        <div class="card shadow mb-4">
+                            <div class="card-header py-3">
+                                <h6 class="m-0 font-weight-bold text-primary text-center">Formulario para agregar una Categoría</h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="row mt-2">
+                                    <div class="col-md-8">
+                                        <label class="labels">Nombre de la Categoría</label>
+                                        <input name="nomcategory" type="text" class="form-control form-control-user" placeholder="Nombre Categoría" autofocus>
+                                    </div>
+                                    <div class="col-md-4 col-sm-10">
+                                        <label class="labels"></label>
+                                        <input type="submit" value="Agregar Categoría" class="btn btn-success btn-user btn-block"></input>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </form>
     </div>
 </body>
