d414f92212c423488e3a9b07f131d7d9f28bd8f3
Acosticahub <acostabuitrago0123@gmail.com>
2021-10-30 22:42:39 -0500

    Frontend Inventario

--- resources/views/h_abastecimiento/inventario.blade.php
+++ resources/views/h_abastecimiento/inventario.blade.php
@@ -3,69 +3,40 @@
 <html>
 
 <head>
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/fc2b074465.js" crossorigin="anonymous"></script>
     <link rel="icon" type="image/png" href="{{ url('img/icon_principal.png') }}">
-    <title>HSI - Productos</title>
+    <title>HSI - Inventario</title>
 </head>
 @section('content')
 
 <body>
-    <div class="container-fluid">
-        <a href="product/new" class="btn btn-primary float-right"><i class="fas fa-plus"></i> Agregar Producto</a>
-        <h1>Gestión de Productos</h1><br>
-        <ul class="nav nav-tabs float-right" id="myTab" role="tablist">
-            <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Activos</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Papelera</button>
-        </ul>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                <table id="example" class="table table-responsive table-bordered table-hover table-striped">
-                    <thead>
-                        <tr>
-                            <th>Nombre</th>
-                            <th>Descripción</th>
-                            <th>Cantidad</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($data as $row)
-                        <tr>
-                            <td>{{$row->nomproduct}}</td>
-                            <td>{{$row->description}}</td>
-                            <td>{{$row->quantity}}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
 
-                <table class="table table-bordered table-hover table-striped">
-                    <thead>
-                        <tr>
-                            <th>Nombre</th>
-                            <th>Descripción</th>
-                            <th>Cantidad</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($data2 as $row)
-                        <tr>
-                            <td>{{$row->nomproduct}}</td>
-                            <td>{{$row->description}}</td>
-                            <td>{{$row->quantity}}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+    <div class="container">
+        <a href="{{ url('product/new') }}" class="btn btn-primary float-right"><i class="fas fa-plus"></i> Agregar Producto</a>
+        <h1>Inventario</h1><br>
+        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <table class="table table-responsive table-bordered table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th style="width: 10%">Nombre</th>
+                        <th style="width: 40%">Descripción</th>
+                        <th style="width: 8%">Cantidad</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($data as $row)
+                    <tr>
+                        <td style="width: 10%">{{$row->nomproduct}}</td>
+                        <td style="width: 40%">{{$row->description}}</td>
+                        <td style="width: 8%">{{$row->quantity}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
+    @stop
 </body>
-@stop
 
 </html>
